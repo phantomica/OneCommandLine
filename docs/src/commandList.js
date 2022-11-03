739e5b05fe0005e41c2a548d4f794e790599d5a8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 21:40:55 +0100

    added comments, fixed issues with CommandHandler, added kill-/exitProcess functions

--- docs/src/commandList.js
+++ docs/src/commandList.js
@@ -1,5 +1,28 @@
-const OCLcommands = require('./commands/OCLcommands');
+const { quit, exit } = require('./src/commands/OCLcommands.js');
+const { killProcess, exitProcess } = require('./src/commands/processControls.js');
 
-const commandList = [OCLcommands.quit, OCLcommands.exit];
+const commandList = []
 
-console.log(commandList);
+//* OCLcommands
+commandList.push({
+    func: quit,
+    funcParam: false,
+    funcProperties: false,
+})
+commandList.push({
+    func: exit,
+    funcParam: false,
+    funcProperties: false,
+});
+
+//* processControls
+commandList.push({
+    func: killProcess,
+    funcParam: true,
+    funcProperties: false,
+})
+commandList.push({
+    func: exitProcess,
+    funcParam: true,
+    funcProperties: false,
+})
---
+++ docs/src/commands/processControls.js
@@ -0,0 +1,22 @@
+const findProcess = require('find-process');
+
+/**
+ * Sends exit signal to given process.
+ * @param {string} processName name of an active process
+ */
+const killProcess = (processName) => {
+    findProcess('name', processName).then((foundProcess) => {
+        if (foundProcess[0] === undefined || null) { console.error("Process not found"); return; }
+        process.kill(foundProcess[0].pid);
+    }, function(err) {
+        console.log(err.stack || err);
+    });
+}
+
+/**
+ * Sends exit signal to given process.
+ * @param {string} processName name of an active process
+ */
+const exitProcess = killProcess;
+
+module.exports = { killProcess, exitProcess }
